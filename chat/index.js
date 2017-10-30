c547030134d53d6b7b21c377bf36b97d7627f604
Kirubel Aklilu <kirubelaklilu1@users.noreply.github.com>
2017-10-30 17:39:34 -0400

    Update index.js

--- chat/index.js
+++ chat/index.js
@@ -1,18 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var num_users = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  num_users++;
+
+  
+  console.log('User Number', "User #" + num_users + " has joined.");
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+  	num_users --;
+    console.log('user disconnected' + num_users);
+  });
   socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
